2392b7cb1e2fafeb76082766806a65974a0e361d
prasad.griet223 <prasadvsrk@icloud.com>
2019-10-23 16:37:33 +0000

    today task 23/10

--- webapp/controller/Dashboard.controller.js
+++ webapp/controller/Dashboard.controller.js
@@ -18,105 +18,105 @@ sap.ui.define(["sap/ui/core/mvc/Controller", "scp/com/saparate/utils/formatter"]
 
 		},
 		_onObjectMatched: function (oEvent) {
-			//var oModel_jobdetails = new sap.ui.model.json.JSONModel();
-			//oModel_jobdetails.loadData(this.getOwnerComponent().getModel("servers").getProperty("latestBuildResults"));
-			var oModel_jobdetails_cpp = new sap.ui.model.json.JSONModel();
-			oModel_jobdetails_cpp.setData(
+			var oModel_jobdetails = new sap.ui.model.json.JSONModel();
+			oModel_jobdetails.loadData(this.getOwnerComponent().getModel("servers").getProperty("latestBuildResults"));
+			//	var oModel_jobdetails_cpp = new sap.ui.model.json.JSONModel();
+			// oModel_jobdetails_cpp.setData(
 
-				[{
-					"id": 36,
-					"name": "test123",
-					"number": 1,
-					"buildResult": "FAILURE",
-					"duartion": 0,
-					"estimatedDuration": 0,
-					"timeStamp": "2019-10-20T16:42:21.000+0000",
-					"triggeredBy": null
-				}, {
-					"id": 35,
-					"name": "demopipeline",
-					"number": 1,
-					"buildResult": "FAILURE",
-					"duartion": 0,
-					"estimatedDuration": 0,
-					"timeStamp": "2019-10-20T08:18:50.000+0000",
-					"triggeredBy": null
-				}, {
-					"id": 34,
-					"name": "testtt",
-					"number": 18,
-					"buildResult": "FAILURE",
-					"duartion": 0,
-					"estimatedDuration": 0,
-					"timeStamp": "2019-10-20T06:31:45.000+0000",
-					"triggeredBy": null
-				}, {
-					"id": 31,
-					"name": "testdd",
-					"number": 1,
-					"buildResult": "FAILURE",
-					"duartion": 0,
-					"estimatedDuration": 0,
-					"timeStamp": "2019-10-19T17:35:41.000+0000",
-					"triggeredBy": null
-				}, {
-					"id": 32,
-					"name": "testtt",
-					"number": 17,
-					"buildResult": "FAILURE",
-					"duartion": 0,
-					"estimatedDuration": 0,
-					"timeStamp": "2019-10-19T17:34:00.000+0000",
-					"triggeredBy": null
-				}, {
-					"id": 33,
-					"name": "testtt",
-					"number": 16,
-					"buildResult": "FAILURE",
-					"duartion": 0,
-					"estimatedDuration": 0,
-					"timeStamp": "2019-10-19T17:32:21.000+0000",
-					"triggeredBy": null
-				}, {
-					"id": 30,
-					"name": "testtt",
-					"number": 15,
-					"buildResult": "FAILURE",
-					"duartion": 0,
-					"estimatedDuration": 0,
-					"timeStamp": "2019-10-19T17:30:36.000+0000",
-					"triggeredBy": null
-				}, {
-					"id": 28,
-					"name": "testtt",
-					"number": 14,
-					"buildResult": "FAILURE",
-					"duartion": 0,
-					"estimatedDuration": 0,
-					"timeStamp": "2019-10-19T17:23:31.000+0000",
-					"triggeredBy": null
-				}, {
-					"id": 29,
-					"name": "testtt",
-					"number": 13,
-					"buildResult": "FAILURE",
-					"duartion": 0,
-					"estimatedDuration": 0,
-					"timeStamp": "2019-10-19T17:21:06.000+0000",
-					"triggeredBy": null
-				}, {
-					"id": 26,
-					"name": "testtt",
-					"number": 12,
-					"buildResult": "FAILURE",
-					"duartion": 0,
-					"estimatedDuration": 0,
-					"timeStamp": "2019-10-19T17:18:21.000+0000",
-					"triggeredBy": null
-				}]
-			);
-			this.getView().setModel(oModel_jobdetails_cpp, "Jobdetails");
-			//	this.getView().setModel(oModel_jobdetails, "Jobdetails");
+			// 	[{
+			// 		"id": 36,
+			// 		"name": "test123",
+			// 		"number": 1,
+			// 		"buildResult": "FAILURE",
+			// 		"duartion": 0,
+			// 		"estimatedDuration": 0,
+			// 		"timeStamp": "2019-10-20T16:42:21.000+0000",
+			// 		"triggeredBy": null
+			// 	}, {
+			// 		"id": 35,
+			// 		"name": "demopipeline",
+			// 		"number": 1,
+			// 		"buildResult": "FAILURE",
+			// 		"duartion": 0,
+			// 		"estimatedDuration": 0,
+			// 		"timeStamp": "2019-10-20T08:18:50.000+0000",
+			// 		"triggeredBy": null
+			// 	}, {
+			// 		"id": 34,
+			// 		"name": "testtt",
+			// 		"number": 18,
+			// 		"buildResult": "FAILURE",
+			// 		"duartion": 0,
+			// 		"estimatedDuration": 0,
+			// 		"timeStamp": "2019-10-20T06:31:45.000+0000",
+			// 		"triggeredBy": null
+			// 	}, {
+			// 		"id": 31,
+			// 		"name": "testdd",
+			// 		"number": 1,
+			// 		"buildResult": "FAILURE",
+			// 		"duartion": 0,
+			// 		"estimatedDuration": 0,
+			// 		"timeStamp": "2019-10-19T17:35:41.000+0000",
+			// 		"triggeredBy": null
+			// 	}, {
+			// 		"id": 32,
+			// 		"name": "testtt",
+			// 		"number": 17,
+			// 		"buildResult": "FAILURE",
+			// 		"duartion": 0,
+			// 		"estimatedDuration": 0,
+			// 		"timeStamp": "2019-10-19T17:34:00.000+0000",
+			// 		"triggeredBy": null
+			// 	}, {
+			// 		"id": 33,
+			// 		"name": "testtt",
+			// 		"number": 16,
+			// 		"buildResult": "FAILURE",
+			// 		"duartion": 0,
+			// 		"estimatedDuration": 0,
+			// 		"timeStamp": "2019-10-19T17:32:21.000+0000",
+			// 		"triggeredBy": null
+			// 	}, {
+			// 		"id": 30,
+			// 		"name": "testtt",
+			// 		"number": 15,
+			// 		"buildResult": "FAILURE",
+			// 		"duartion": 0,
+			// 		"estimatedDuration": 0,
+			// 		"timeStamp": "2019-10-19T17:30:36.000+0000",
+			// 		"triggeredBy": null
+			// 	}, {
+			// 		"id": 28,
+			// 		"name": "testtt",
+			// 		"number": 14,
+			// 		"buildResult": "FAILURE",
+			// 		"duartion": 0,
+			// 		"estimatedDuration": 0,
+			// 		"timeStamp": "2019-10-19T17:23:31.000+0000",
+			// 		"triggeredBy": null
+			// 	}, {
+			// 		"id": 29,
+			// 		"name": "testtt",
+			// 		"number": 13,
+			// 		"buildResult": "FAILURE",
+			// 		"duartion": 0,
+			// 		"estimatedDuration": 0,
+			// 		"timeStamp": "2019-10-19T17:21:06.000+0000",
+			// 		"triggeredBy": null
+			// 	}, {
+			// 		"id": 26,
+			// 		"name": "testtt",
+			// 		"number": 12,
+			// 		"buildResult": "FAILURE",
+			// 		"duartion": 0,
+			// 		"estimatedDuration": 0,
+			// 		"timeStamp": "2019-10-19T17:18:21.000+0000",
+			// 		"triggeredBy": null
+			// 	}]
+			// );
+			//	this.getView().setModel(oModel_jobdetails_cpp, "Jobdetails");
+			this.getView().setModel(oModel_jobdetails, "Jobdetails");
 			this.byId("idBuildstblHdr").setText("Recent Builds");
 		},
 		handleSelectionChange: function (oEvent) {
--- webapp/controller/buildStages.controller.js
+++ webapp/controller/buildStages.controller.js
@@ -44,50 +44,63 @@ sap.ui.define([
 		_onObjectMatched: function (oEvent) {
 			var jobId = oEvent.getParameter("arguments").jobId;
 			var buildId = oEvent.getParameter("arguments").buildid;
-			//	var oModel_buildstatusdetails = new sap.ui.model.json.JSONModel();
-			//	oModel_buildstatusdetails.loadData(this.getOwnerComponent().getModel("servers").getProperty("JobStageResults") + "?jobName=" + jobId+"&buildNumber="+buildId)  ;
-			var oModel_buildstatusdetails_cpp = new sap.ui.model.json.JSONModel();
-			oModel_buildstatusdetails_cpp.setData([{
-				"name": "#1",
-				"duartion": 0,
-				"estimatedDuration": 0,
-				"number": 1,
-				"buildResult": "FAILURE",
-				"testResult": null,
-				"testReport": null,
-				"timeStamp": "2019/10/22 22:55:50",
-				"stageResult": {
-					"stages": [{
-						"id": 7,
-						"name": "prepare----000",
-						"execNode": "",
-						"status": "SUCCESS",
-						"startTime": "2019/10/22 22:56:02",
-						"duration": 0,
-						"pauseDuration": 0
-					}, {
-						"id": 12,
-						"name": "prepareConfigYaml",
-						"execNode": "",
-						"status": "SUCCESS",
-						"startTime": "2019/10/22 22:56:04",
-						"duration": 0,
-						"pauseDuration": 0
-					}, {
-						"id": 17,
-						"name": "prepareBuild",
-						"execNode": "",
-						"status": "FAILED",
-						"startTime": "2019/10/22 22:56:04",
-						"duration": 0,
-						"pauseDuration": 0
-					}]
-				}
-			}]);
-			this.getView().setModel(oModel_buildstatusdetails_cpp, "Jobstatusdetails");
-			//this.getView().setModel(oModel_buildstatusdetails, "Jobstatusdetails");
+			var oModel_buildstatusdetails = new sap.ui.model.json.JSONModel();
+			oModel_buildstatusdetails.loadData(this.getOwnerComponent().getModel("servers").getProperty("JobStageResults") + "?jobName=" +
+				jobId + "&buildNumber=" + buildId);
+			// var oModel_buildstatusdetails_cpp = new sap.ui.model.json.JSONModel();
+			// oModel_buildstatusdetails_cpp.setData([{
+			// 	"name": "#1",
+			// 	"duartion": 0,
+			// 	"estimatedDuration": 0,
+			// 	"number": 1,
+			// 	"buildResult": "FAILURE",
+			// 	"testResult": null,
+			// 	"testReport": null,
+			// 	"timeStamp": "2019/10/22 22:55:50",
+			// 	"stageResult": {
+			// 		"stages": [{
+			// 			"id": 7,
+			// 			"name": "prepare----000",
+			// 			"execNode": "",
+			// 			"status": "SUCCESS",
+			// 			"startTime": "2019/10/22 22:56:02",
+			// 			"duration": 0,
+			// 			"pauseDuration": 0
+			// 		}, {
+			// 			"id": 12,
+			// 			"name": "prepareConfigYaml",
+			// 			"execNode": "",
+			// 			"status": "SUCCESS",
+			// 			"startTime": "2019/10/22 22:56:04",
+			// 			"duration": 0,
+			// 			"pauseDuration": 0
+			// 		}, {
+			// 			"id": 17,
+			// 			"name": "prepareBuild",
+			// 			"execNode": "",
+			// 			"status": "FAILED",
+			// 			"startTime": "2019/10/22 22:56:04",
+			// 			"duration": 0,
+			// 			"pauseDuration": 0
+			// 		}]
+			// 	}
+			// }]);
+			// this.getView().setModel(oModel_buildstatusdetails_cpp, "Jobstatusdetails");
+			this.getView().setModel(oModel_buildstatusdetails, "Jobstatusdetails");
 			this.byId("idBuildStages").setTitle("Stagewise Build Results--" + jobId + "--Build#" + buildId);
-
+			var oModel_buildstageslog = new sap.ui.model.json.JSONModel();
+			oModel_buildstageslog.loadData(this.getOwnerComponent().getModel("servers").getProperty("log") + "?jobName=" +
+				jobId + "&buildNumber=" + buildId);
+			oModel_buildstageslog.attachRequestCompleted(function () {
+				var p = this.byId("idlog_content");
+				p.removeAllContent();
+				var sResponse = oModel_buildstageslog.getData()["response"];
+				var r = JSON.stringify(sResponse).replace(/\\r\\n/g, "<br />");
+				var oText2 = new sap.ui.core.HTML();
+				oText2.setContent("<div>" + r + " </div>");
+				oText2.placeAt(this.byId("idlog_content"));
+				this.getView().setBusy(false);
+			}.bind(this));
 		}
 	});
 
